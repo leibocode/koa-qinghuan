62fc7638938513c05f7e3e8cea6c0e48bbd27afc
leibo <252517584@qq.com>
2019-06-27 11:50:18 +0800

    中间件统一注入

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const koa = require('koa')
+const R = require('ramda')
+const { resolve } = require('path')
 
+const r = path => resolve(__dirname,path)
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+const MiddleWares = ['database','common','router']
 /**
  * 应用启动入口
  *
@@ -8,14 +14,19 @@ const koa = require('koa')
 class StartUp {
     constructor(){
         this.app = new koa()
-        this.useMIddwares()
+        this.useMiddleWares(this.app)(MiddleWares)
     }
     useMiddleWares(app){
         //注入中间件
-        
+        return R.map(R.compose(
+            R.map(i=>i(app)),
+            require,
+            i=>`${r('./middwares')}/${i}` 
+        ))
     }
     async start(){
-        
+        this.app.listen(port,host) 
+        console.log(`API端启动成功,端口号${port}`)  
     }
 }
 
