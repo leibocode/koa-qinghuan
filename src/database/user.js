682c91a691c4a75113fa7e12c889a4d52b9e9a95
leibo <252517584@qq.com>
2019-06-28 12:02:57 +0800

    创建user模型

---
+++ src/database/user.js
@@ -0,0 +1,14 @@
+const {
+    Sequelize,
+    Model
+} = require('sequelize')
+
+export default class User extends Model {
+    static async getUserByOpenid(openid){
+
+    }
+}
+
+User.init({
+
+})
