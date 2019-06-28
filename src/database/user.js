8262c854ea006a35c2bf28d6e34d3c7980d09083
leibo <252517584@qq.com>
2019-06-28 13:53:21 +0800

    update

--- src/database/user.js
+++ src/database/user.js
@@ -4,11 +4,45 @@ const {
 } = require('sequelize')
 
 export default class User extends Model {
-    static async getUserByOpenid(openid){
 
+    static async verifyEmailPassword(email,plainPassword){
+        
+    }
+
+    static async getUserByOpenid(openid){
+        const user = await User.findOne({
+            where:{
+                openid
+            }
+        })
+        return user
+    }
+    static async registerByOpenid(openid){
+        return await User.create({
+            openid
+        })
     }
 }
 
 User.init({
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+    nickname:Sequelize.STRING,
+    email:{
+        type:Sequelize.STRING(20),
+        unique:true
+    },
+    password:{
 
+    },
+    openid:{
+        type:Sequelize.STRING(64),
+        unique:true
+    }
+},{
+    sequelize,
+    tableName:'user'
 })
