18907ebe0a31ae610e429294f200b696aadee1f1
leibo <252517584@qq.com>
2019-07-10 16:28:16 +0800

    评论服务

--- src/services/art.js
+++ src/services/art.js
@@ -5,23 +5,53 @@ const Db = require('../database/index')
 const Movie = Db.getModel('movie')
 const Sentence = Db.getModel('sentence')
 const Music = Db.getModel('music')
+const Favor = Db.getModel('favor')
 
-class Art {
+export default class Art {
     constructor(art_id,type) {
         this.art_id = art_id
         this.type = type
     }
 
     static async getDetail(uid) {
-        
+
     }
 
     static async _getListByType(ids,type){
         
     }
 
-    static async getList(){
+    static async getList(artInfoList){
+        const artInfoObj = {
+            100:[],
+            200:[],
+            300:[]
+        }
+        for(let artInfo of artInfoList){
 
+        }
     }
 
+    static async getData(art_id,type){
+        let art = null
+        const finder = {
+            where:{
+                id:art_id
+            }
+        }
+        switch(type){
+            case 100:
+                art = await Movie.findOne(finder)
+                break;
+            case 200:
+                art = await Music.findOne(finder)
+                break;
+            case 300:
+                art = await Sentence.findOne(finder)
+                break;
+            default:
+                break;
+        }
+        return art;
+    }
 }
---
+++ src/services/comment.js
@@ -0,0 +1,37 @@
+
+const { flatten } = require('lodash')
+const { Op } =require('sequelize')
+const Db = require('../database/index')
+
+const commentModel = Db.getModel()
+
+class Comment {
+    static async addComment (bookId,content){
+        const comment = await commentModel.findOne({
+            where:{
+                book_id:bookId,
+                content
+            }
+        })
+        if(!comment){
+            return await commentModel.create({
+                book_id:bookId,
+                content,
+                nums:1
+            })
+        }else {
+            return await comment.increment('nums',{
+                by:1
+            })
+        }
+    }
+
+    static async getCommentsByBookId(bookId){
+        const comments = await Comment.findAll({
+            where:{
+                book_id:bookId
+            }
+        })
+        return comments
+    }
+}
